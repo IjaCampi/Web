ae8954450035ce16e3a9aa254e4e2b9338e6831c
brahimABD98 <brahim.abderrahim@esprit.tn>
2022-04-19 15:53:31 +0100

    equipement front

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -8,9 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
 use Symfony\Component\Routing\Annotation\Route;
-
 /**
  * @Route("/commande")
  */
